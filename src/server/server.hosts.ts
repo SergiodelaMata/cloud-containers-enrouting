3fa309c9c850e385b3332f1be165156b48578034
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-12 21:19:29 +0100

    :sparkles: Update hosts for containers.

--- src/server/server.hosts.ts
+++ src/server/server.hosts.ts
@@ -1,6 +1,6 @@
 export enum Hosts {
-    RedisLogin = "localhost",
-    Products = "localhost",
-    Transactions = "localhost",
-    Users = "localhost"
+    RedisLogin = "cloud-redis-v2",
+    Products = "cloud-products-v2",
+    Transactions = "cloud-transactions-v2",
+    Users = "cloud-users-v2"
 }
