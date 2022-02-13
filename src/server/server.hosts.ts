d0b23125aefd1cd7bdf806753a67628147c3bf24
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-13 17:29:56 +0100

    :sparkles: Updated hosts to be used with Kubernetes services built.

--- src/server/server.hosts.ts
+++ src/server/server.hosts.ts
@@ -1,6 +1,6 @@
 export enum Hosts {
-    RedisLogin = "cloud-redis-v2",
-    Products = "cloud-products-v2",
-    Transactions = "cloud-transactions-v2",
-    Users = "cloud-users-v2"
+    RedisLogin = "redis-service",
+    Products = "products",
+    Transactions = "transactions",
+    Users = "users"
 }
